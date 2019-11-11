b92c9097c0d9578f3677fa1727f800961e826f57
Geert Eltink <geert.eltink@gmail.com>
2019-11-11 16:32:25 +0100

    feat: add debug arg

--- src/index.js
+++ src/index.js
@@ -6,12 +6,19 @@ try {
   const command = core.getInput('command');
   console.log(`Executing '${command}' command`);
 
+  const debug = core.getInput('debug') === 'true';
+  if (debug) {
+    console.log('Enabling debug mode');
+  }
+
   const time = (new Date()).toTimeString();
   core.setOutput('time', time);
 
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  if (debug) {
+    console.log(`The event payload: ${payload}`);
+  }
 } catch (error) {
   core.setFailed(error.message);
 }
