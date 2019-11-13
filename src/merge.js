61f62cf460ee8bbd2bcac131d967871385a8c555
Geert Eltink <geert.eltink@gmail.com>
2019-11-13 10:41:19 +0100

    fix: cleanup code

--- src/merge.js
+++ src/merge.js
@@ -49,51 +49,6 @@ const run = async () => {
   });
 
   console.log(JSON.stringify(pr, undefined, 2));
-
-  /*
-  console.log('processing pull request');
-  for (const pullRequest of context.payload.check_suite.pull_requests) {
-    // const pullRequestId = pullRequest.id;
-    const pull_number = pullRequest.number;
-    core.info(`pull request detected: ${pull_number}`);
-
-    const pr = await github.pulls.get({
-      owner,
-      repo,
-      pull_number,
-    });
-
-    console.log(JSON.stringify(pr, undefined, 2));
-  }
-  */
-  /*
-    // Dump event data first
-    console.log(JSON.stringify(github.context, undefined, 2));
-
-    // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
-    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-    const octokit = new github.GitHub(GITHUB_TOKEN);
-
-
-    if (github.context.payload.check_suite.pull_requests === undefined) {
-      console.log('Skipping: pull request information is unavailable.');
-      return;
-    }
-
-    for (const pullRequest of github.context.payload.check_suite.pull_requests) {
-      // const pullRequestId = pullRequest.id;
-      const pullRequestNumber = pullRequest.number;
-      console.log(JSON.stringify(pullRequest, undefined, 2));
-
-      const pr = await octokit.pulls.get({
-        owner,
-        repo,
-        pullRequestNumber,
-      });
-
-      console.log(JSON.stringify(pr, undefined, 2));
-    }
-  */
 };
 
 module.exports = run;
